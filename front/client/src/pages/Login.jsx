f07d87f53f331868c64edf2bb9eb32f54c2689bc
Thiago Lopes <tiagojosecnn@outlook.com>
2024-09-15 18:53:40 -0300

    adiciona estilo ao componente Register.

--- front/client/src/pages/Login.jsx
+++ front/client/src/pages/Login.jsx
@@ -1,34 +1,63 @@
 import React from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function Login() {
+    const[values, setvalue] = useState({
+        email: "",
+        password: "",
+    });
+    const [msgErroEmail, setMsgErroEmail] = useState('');
+    const [msgErroPassword, setMsgErroPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        handleValidation();
+    }
+
+    const handleValidation = () => {
+        const {email, password} = values;
+        const regex = /^[^\s@]+@[^\s@]+\.[a-z]{2,}$/;
+
+        if (email === '' || !regex.test(email.toLowerCase())) {
+            setMsgErroEmail('Esse e-mail é inválido.');
+            return false;
+        }
+
+        if(password === '' || password.length < 4){
+            setMsgErroPassword('Senha muito curta!');
+            return false;
+        }
+
+        return true;
+
+    }
+
+    const handleChange = (event) => {
+        setvalue({...values,[event.target.name]: event.target.value,});
+    }
     return (
         <div className="formContainer">
-            <Formik initialValues={{}}>
-                <Form className="form-login-group">
-                    <h1>Chat</h1>
-                    <div className="flex-column">
-                        <label htmlFor="email">Email</label>
-                        <Field id="email" name="email" className="form-field" placeholder="name@host.com" />
-                        <ErrorMessage
-                            component="span"
-                            name="email"
-                            className="form-error" 
-                        />
-                    </div>
-                    <div className="flex-column">
-                        <label htmlFor="password">Password</label>
-                        <Field id="password" name="password" className="form-field" type="password" placeholder="Password"/>
-                        <ErrorMessage 
-                            component="span"
-                            name="password"
-                            className="form-erroe"
-                        />
-                    </div>
+            
+            <form className="form-login-group" onSubmit={(event) => handleSubmit(event)}>
+                <h1>Chat</h1>
+                <div className="flex-column">
+                    <label htmlFor="email">Email</label>
+                    <input id="email" name="email" className="form-field" placeholder="name@host.com" onChange={(e) => {handleChange(e), setMsgErroEmail('')}}/>
+                    <span className="msgError">{msgErroEmail}</span>
+                </div>
+                <div className="flex-column">
+                    <label htmlFor="password">Password</label>
+                    <input id="password" name="password" className="form-field" type="password" placeholder="Password" onChange={(e) => {handleChange(e), setMsgErroPassword('')}}/>
+                    <span className="msgError">{msgErroPassword}</span>
+                </div>
 
-                    <button className="button-submit-form" type="submit">Sign in</button>
-                </Form>
-            </Formik>
+                <button className="button-submit-form" type="submit">Sign in</button>
+
+                <div>
+                    <span>Precisa de uma conta? <Link to={'/register'}>Registre-se</Link> </span>
+                </div>
+            </form>
         </div>
     );
 }
--- front/client/src/pages/Register.jsx
+++ front/client/src/pages/Register.jsx
@@ -1,9 +1,88 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function Register() {
+    const[values, setvalue] = useState({
+        email: "",
+        username: "",
+        password: "",
+        confirmPassword: ""
+    });
+    const [msgErroEmail, setMsgErroEmail] = useState('');
+    const [msgErroPassword, setMsgErroPassword] = useState('');
+    const [msgErroUsername, setMsgErroUsername] = useState('');
+    const [msgErroConfirmPassword, setMsgErroConfirmPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        handleValidation();
+    }
+
+    const handleValidation = () => {
+        const {email, username, password, confirmPassword} = values;
+        const regex = /^[^\s@]+@[^\s@]+\.[a-z]{2,}$/;
+
+
+        if(username === '' || username.length < 3){
+            setMsgErroUsername('Nome de usuário muito curto!');
+            return false;
+        }
+        
+        if (email === '' || !regex.test(email.toLowerCase())) {
+            setMsgErroEmail('Esse e-mail é inválido.');
+            return false;
+        }
+
+        if(password === '' || password.length < 4){
+            setMsgErroPassword('Senha muito curta!');
+            return false;
+        }
+
+        if(confirmPassword !== password){
+            setMsgErroConfirmPassword('As senhas não coincidem!');
+            return false;
+        }
+
+        return true;
+
+    }
+
+    const handleChange = (event) => {
+        setvalue({...values,[event.target.name]: event.target.value,});
+    }
+
     return (
-        <>
-            <h1>Register</h1>
-        </>
+        <div className="formContainer">         
+            <form className="form-login-group" onSubmit={(event) => handleSubmit(event)}>
+                <h1>Chat</h1>
+                <div className="flex-column">
+                    <label htmlFor="username">Username</label>
+                    <input id="username" name="username" className="form-field" placeholder="username" onChange={(e) => {handleChange(e), setMsgErroUsername('')}}/>
+                    <span className="msgError">{msgErroUsername}</span>
+                </div>
+                <div className="flex-column">
+                    <label htmlFor="email">Email</label>
+                    <input id="email" name="email" className="form-field" placeholder="name@host.com" onChange={(e) => {handleChange(e), setMsgErroEmail('')}}/>
+                    <span className="msgError">{msgErroEmail}</span>
+                </div>
+                <div className="flex-column">
+                    <label htmlFor="password">Password</label>
+                    <input id="password" name="password" className="form-field" type="password" placeholder="Password" onChange={(e) => {handleChange(e), setMsgErroPassword('')}}/>
+                    <span className="msgError">{msgErroPassword}</span>
+                </div>
+                <div className="flex-column">
+                    <label htmlFor="confirmPassword">Confirm Password</label>
+                    <input id="confirmPassword" name="confirmPassword" className="form-field" type="password" placeholder="Confirm Password" onChange={(e) => {handleChange(e), setMsgErroConfirmPassword('')}}/>
+                    <span className="msgError">{msgErroConfirmPassword}</span>
+                </div>
+
+                <button className="button-submit-form" type="submit">Sign up</button>
+
+                <div>
+                    <span>Já possui uma conta? <Link to={'/login'}>Login</Link> </span>
+                </div>
+            </form>
+        </div>
     )
 }
