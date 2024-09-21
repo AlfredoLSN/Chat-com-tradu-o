66f8aab3fd5f0c91af117e2af8f8790d2ab65102
Alfredo Lucas <alfredoliu14@live.com>
2024-09-21 11:39:11 -0300

    adiciona salas no login

--- back/models/Room.js
+++ back/models/Room.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const roomSchema = new mongoose.Schema({
     name: { type: String, required: true, unique: true },
-    users: { type: [String], default: [] },
+    users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
 });
 
 const Room = mongoose.model("Room", roomSchema);
