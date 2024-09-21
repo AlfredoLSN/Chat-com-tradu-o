b770bbed37835b34ed38ac9913c2ad18ea32d4e0
Alfredo Lucas <alfredoliu14@live.com>
2024-09-21 13:29:49 -0300

    configura body express2

--- back/server.js
+++ back/server.js
@@ -14,6 +14,7 @@ const io = new Server(server);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 mongoose
     .connect("mongodb://localhost:27017/chat")
     .then(() => {
